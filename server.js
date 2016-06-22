d3814d4d5ef2c3c3c3297945747bcf72249ed067
Pashtidot <Ronhagafny20@gmail.com>
2016-06-22 22:27:27 +0300

    added favicon

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const app = express();
 const path = require('path');
 const dateService = require('./dateService.js');
 const logicService = require('./logicService.js');
+const favicon = require('serve-favicon');
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get('/', (req, res) => {
     res.redirect(`/${dateService.getCurrentDate()}`);
@@ -17,7 +19,7 @@ app.get('/', (req, res) => {
 
 app.get('/:month/:day', (req, res) => {
     dateService.isBizCaz(req.params.month, req.params.day, (isBizCaz) => {
-        let data = logicService.getData(req.params.month, req.params.day, isBizCaz);
+        let data = logicService.getData(req.params.month, isBizCaz);
         res.render('index', data);
     });
 });
